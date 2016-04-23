8fd4db8c8968a307473d535c5767463480648d12
gokulmurali <gokulkmurali@gmail.com>
2016-04-23 17:39:09 +0530

    Update 2.9.c

--- chapter2/2.9.c
+++ chapter2/2.9.c
@@ -6,7 +6,10 @@
  int bitcount(unsigned x){
  	int b;
 
-	/*x = x&(x-1) deletes rightmost bit, since x and x-1 differ only by one bit (lsb, rightmost)*/
+	/*x = x&(x-1) deletes rightmost bit, since x and x-1 differ only by one bit (lsb, rightmost) or 'n' bits of 
+	x contains one '1' followed by all zeroes while (x-1) conatins 'n-1' ones making x&(x-1) = 0 
+	eg: 8&7 where 8 = 1000 and 7 = 111, 8&7 = 0 and bit count function gives 1 as the answer for 
+	number of bits in 8*/
 
  	for (b = 0; x != 0; x &= (x-1)){ 
 		//printf("%d\n", x);		 	 
